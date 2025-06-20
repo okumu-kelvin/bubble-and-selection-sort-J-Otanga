b0a77bf496ffdf032cf0c0e36e720c374da477d3
J-Otanga <furahaotanga@gmail.com>
2025-06-20 12:13:14 +0300

    1st Update

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,6 +1,6 @@
 # Bubble sort function that sorts the list step by step
-def bubble_sort(data):
-    n = len(data)  # Get the number of items in the list
+def bubble_sort(unsorted_list):
+    n = len(unsorted_list)  # Get the number of items in the list
 
     # Loop through the list multiple times
     for i in range(n):
@@ -9,17 +9,19 @@ def bubble_sort(data):
         for j in range(0, n - i - 1):
 
             # If the current number is bigger than the one next to it, swap them
-            if data[j] > data[j + 1]:
-                data[j], data[j + 1] = data[j + 1], data[j]
+            if unsorted_list[j] > unsorted_list[j + 1]:
+                unsorted_list[j], unsorted_list[j + 1] = unsorted_list[j + 1], unsorted_list[j]
 
-    return data  # Give back the sorted list
-
+    return unsorted_list  # Give back the sorted list
+def reverse_array(arr):
+    return arr[::-1]  
 
 # My list that I want to sort
-data = [5, 3, 2, 4, 4, 1]
 
+arr = [5, 3, 2, 4, 4, 1]
 # Call the function to sort it
-sorted_array = bubble_sort(data)
-
+sorted_array = bubble_sort(arr)
 # Print the sorted version
 print("Sorted array is:", sorted_array)
+# Print the reversed version
+print("Reversed array is:",reverse_array(arr) )  # Should show the list backwards
